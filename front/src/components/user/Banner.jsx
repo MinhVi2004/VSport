b0f38f0a327e06abd2cef463b9222a98bee5bfa1
Dương Văn Minh Vi <141652938+MinhVi2004@users.noreply.github.com>
2026-02-05 10:38:37 +0700

    Update Banner.jsx

--- front/src/components/user/Banner.jsx
+++ front/src/components/user/Banner.jsx
@@ -66,7 +66,7 @@ const Banner = () => {
             <img
               src={slide.image}
               alt={`Banner ${index + 1}`}
-              className="w-full h-full object-fill"
+              className="w-full h-full object-cover"
             />
           </div>
         ))}
