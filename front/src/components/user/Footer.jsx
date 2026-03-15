1885b33bc438c4eba6e3ba2357574cd6a396aa06
Dương Văn Minh Vi <141652938+MinhVi2004@users.noreply.github.com>
2026-03-15 17:53:32 +0700

    Update Footer.jsx

--- front/src/components/user/Footer.jsx
+++ front/src/components/user/Footer.jsx
@@ -2,8 +2,8 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-gray-300">
       <div className="container mx-auto px-4 py-6 text-center">
-        <p>&copy; {new Date().getFullYear()} MyWebsite. All rights reserved.</p>
-        <p className="text-sm">Địa chỉ: An Dương Vương, Quận 5, TP.HCM</p>
+        <p>&copy; {new Date().getFullYear()} VSport. Owner: Dương Văn Minh Vi.</p>
+        <p className="text-sm">Địa chỉ: TP.HCM</p>
       </div>
     </footer>
   );
